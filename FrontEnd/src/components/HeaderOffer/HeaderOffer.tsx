4d7dfa46da5b907d0a6ad60b083adfa9276ee87a
Taras <turchenko730@gmail.com>
2024-06-09 19:26:28 +0300

    refactor calculation of time func

--- FrontEnd/src/components/HeaderOffer/HeaderOffer.tsx
+++ FrontEnd/src/components/HeaderOffer/HeaderOffer.tsx
@@ -1,30 +1,30 @@
 import styles from "./HeaderOffer.module.css"
 import { useEffect, useState } from "react"
-//rewrite to use callBack
+
+function calculateTime(timer: number) {
+  const endDate = new Date("2026-06-30T00:00:00").getTime()
+  const now = Date.now()
+  const timeDiff = endDate - now
+  if (timeDiff < 0) {
+    clearInterval(timer)
+    return "Розпродаж закінчено"
+  }
+  const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24))
+  const hours = Math.floor(
+    (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  )
+  const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60))
+  const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000)
+
+  const countdownText = `До кінця розпродажу ${days}д. ${hours}год. ${minutes}хв. ${seconds}сек. Дізнатись більше`
+  return countdownText
+}
+
 const HeaderOffer = () => {
   const [counterText, setCounterText] = useState(``)
   useEffect(() => {
-    function calculateTime() {
-      const endDate = new Date("2026-06-30T00:00:00").getTime()
-      const now = Date.now()
-      const timeDiff = endDate - now
-      if (timeDiff < 0) {
-        clearInterval(timer)
-        setCounterText("Розпродаж закінчена")
-        return
-      }
-      const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24))
-      const hours = Math.floor(
-        (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      )
-      const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60))
-      const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000)
-
-      const countdownText = `До кінця розпродажу ${days}д. ${hours}год. ${minutes}хв. ${seconds}сек. Дізнатись більше`
-      setCounterText(countdownText)
-    }
-    calculateTime()
-    const timer = setInterval(calculateTime, 1000)
+    const timer = setInterval(() => setCounterText(calculateTime(timer)), 1000)
+    setCounterText(calculateTime(timer))
     return () => clearInterval(timer)
   }, [])
   return (
