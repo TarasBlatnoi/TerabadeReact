ea779a9396b969340ccdd0a1fecc50ff4359caf3
Moriarty <116695498+Moriartymath@users.noreply.github.com>
2024-06-23 01:14:36 +0300

    Added basic structure for footer

---
+++ FrontEnd/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import FooterItemList from "./FooterItemList/FooterItemList"
+
+const Footer = () => {
+  return (
+    <div>
+      <FooterItemList />
+    </div>
+  )
+}
+
+export default Footer
---
+++ FrontEnd/src/components/Footer/FooterItemList/FooterItemList.tsx
@@ -0,0 +1,7 @@
+import styles from "./FooterItemList.module.css"
+
+const FooterItemList = function () {
+  return <ul className={styles.list}></ul>
+}
+
+export { FooterItemList as default }
