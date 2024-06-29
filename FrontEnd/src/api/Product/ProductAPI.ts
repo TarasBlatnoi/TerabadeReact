de6dd8dd1b91f91ac49dafed375819dd48a5e29b
Taras Yurchenko <turchenko730@gmail.com>
2024-06-29 19:52:46 +0300

    added ProductAPI

---
+++ FrontEnd/src/api/Product/ProductAPI.ts
@@ -0,0 +1,20 @@
+import { client } from "../instance"
+
+class ProductAPI {
+  async getAll() {
+    const { data } = await client.get(`/products`)
+    return data
+  }
+
+  async getMenProducts() {
+    const { data } = await client.get(`/products/men`)
+    return data
+  }
+
+  async getById(id: string) {
+    const { data } = await client.get(`/products/${id}`)
+    return data
+  }
+}
+
+export default new ProductAPI()
