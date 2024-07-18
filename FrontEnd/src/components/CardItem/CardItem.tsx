6ce1d4f0356076601008bf1c69fe2c329920065b
Taras Yurchenko <turchenko730@gmail.com>
2024-07-18 17:02:42 +0300

    feat: added basic functionality for detailed Product page

--- FrontEnd/src/components/CardItem/CardItem.tsx
+++ FrontEnd/src/components/CardItem/CardItem.tsx
@@ -1,5 +1,6 @@
 import styles from "./CardItem.module.css"
 import { ProductType } from "../../types/Product"
+import { Link } from "react-router-dom"
 
 type CardItemProps = {
   product: ProductType
@@ -9,17 +10,19 @@ const CardItem = ({ product }: CardItemProps) => {
   function handleClick() {}
   return (
     <div className={styles.item}>
-      <h3 style={{ margin: "auto", width: "80%", textAlign: "center" }}>
-        {product.name}
-      </h3>
-      <img
-        src={`data:image/jpeg;base64,${product.image}`}
-        alt={product.name}
-        style={{ width: "90%", height: "80%" }}
-      />
-      <button onClick={handleClick}>
-        <i> додати в кошик</i>
-      </button>
+      <Link to={`${product.ProductID}`}>
+        <h3 style={{ margin: "auto", width: "80%", textAlign: "center" }}>
+          {product.name}
+        </h3>
+        <img
+          src={`data:image/jpeg;base64,${product.image}`}
+          alt={product.name}
+          style={{ width: "90%", height: "80%" }}
+        />
+        <button onClick={handleClick}>
+          <i> додати в кошик</i>
+        </button>
+      </Link>
     </div>
   )
 }
