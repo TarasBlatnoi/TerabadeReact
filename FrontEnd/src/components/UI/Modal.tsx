f389e29c22cddea3f99755f3caadb4194ba6ab14
Taras Yurchenko <turchenko730@gmail.com>
2024-07-19 13:24:21 +0300

    feat: put logic of closing cart from outside with custom hook

--- FrontEnd/src/components/UI/Modal.tsx
+++ FrontEnd/src/components/UI/Modal.tsx
@@ -1,6 +1,7 @@
-import { ReactNode, useContext, useEffect, useRef } from "react"
+import { ReactNode, RefObject, useContext } from "react"
 import { createPortal } from "react-dom"
 import { CartContext } from "../../context/CartContext"
+import useOutsideClick from "../../hooks/useOutsideClick"
 
 interface ModalProps {
   open: boolean
@@ -10,28 +11,15 @@ interface ModalProps {
 
 const Modal = ({ open, children, className = "" }: ModalProps) => {
   const { closeCart } = useContext(CartContext)
-  const dialog = useRef<HTMLDialogElement>(null)
-  useEffect(() => {
-    function closeDialog(e: Event) {
-      const target = e.target as HTMLElement
-      console.log(dialog.current?.contains(target))
-      if (!dialog.current?.contains(target)) {
-        console.log("outside click")
-        closeCart()
-      }
-    }
-    document.addEventListener("click", closeDialog, true)
-    return () => {
-      document.removeEventListener("click", closeDialog, true)
-    }
-  }, [closeCart])
+
+  const ref = useOutsideClick(closeCart) as RefObject<HTMLDialogElement>
   const modalRoot = document.getElementById("modal")
   if (!modalRoot) {
     return null
   }
   return createPortal(
     <dialog
-      ref={dialog}
+      ref={ref}
       className={`${className}`}
       open={open}
       style={{ zIndex: "300" }}
