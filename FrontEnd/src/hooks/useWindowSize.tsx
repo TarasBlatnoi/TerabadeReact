f23acb764c2dc266dfa1c8d4360a2a0e51359cea
Taras Yurchenko <turchenko730@gmail.com>
2024-07-30 23:57:35 +0300

    feat: added useWindowSize custom hook

---
+++ FrontEnd/src/hooks/useWindowSize.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react"
+
+const useWindowSize = () => {
+  const [screenSize, setScreenSize] = useState(window.innerWidth)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize(window.innerWidth)
+    }
+
+    window.addEventListener("resize", handleResize)
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  }, [])
+  return screenSize
+}
+
+export default useWindowSize
