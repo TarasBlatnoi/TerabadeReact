c69f0feac61d1491b5143a3165ca92c04d3b70b1
Taras Yurchenko <turchenko730@gmail.com>
2024-08-04 14:20:35 +0300

    fix: fixed changind state when cookie exires

--- FrontEnd/src/utils/checkAuthLoader.ts
+++ FrontEnd/src/utils/checkAuthLoader.ts
@@ -5,7 +5,7 @@ async function loader() {
   try {
     const res = await AuthAPI.checkAuth()
     if (!res.authenticated) {
-      return redirect("/login")
+      return redirect("/login?loggedIn=false")
     }
     return null
   } catch (err) {
