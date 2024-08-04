f4baa8ca5cae1eb7a29e057c0265dce94b64727f
Taras Yurchenko <turchenko730@gmail.com>
2024-08-04 22:12:13 +0300

    fix: fixed hero section css

--- FrontEnd/src/components/UI/Button/Button.tsx
+++ FrontEnd/src/components/UI/Button/Button.tsx
@@ -4,19 +4,21 @@ import styles from "./Button.module.css"
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "primary" | "secondary"
   disabled?: boolean
+  className?: string
 }
 
 const Button = ({
   variant = "primary",
   children,
   disabled,
+  className,
   ...props
 }: ButtonProps) => {
   return (
     <button
       className={`${styles.button} ${styles[variant]} ${
         disabled ? "disabled" : ""
-      }`}
+      } ${className}`}
       disabled={disabled}
       {...props}
     >
