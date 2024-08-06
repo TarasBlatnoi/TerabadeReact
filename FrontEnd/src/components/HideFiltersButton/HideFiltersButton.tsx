9a323301a46ad5b7493f201bfa2d3441680c1dc7
Moriarty <116695498+Moriartymath@users.noreply.github.com>
2024-08-06 14:23:54 +0300

    Add sort context, and more css

--- FrontEnd/src/components/HideFiltersButton/HideFiltersButton.tsx
+++ FrontEnd/src/components/HideFiltersButton/HideFiltersButton.tsx
@@ -1,15 +1,17 @@
 import styles from "./HideFiltersButton.module.css"
-
+import filterSVG from "../../assets/images/Filter.svg"
+import { MouseEventHandler } from "react"
 type HideFiltersButtonProps = {
-  onClick: Function
+  onClick: MouseEventHandler<HTMLDivElement>
 }
 
 function HideFiltersButton({ onClick }: HideFiltersButtonProps) {
   return (
-    <div className={styles.container}>
-      <button className={styles.button} onClick={() => onClick()}>
-        Hide filters
-      </button>
+    <div className={styles.container} onClick={onClick}>
+      <h2>Приховати фільтр</h2>
+      <div className={styles.svgContainer}>
+        <img src={filterSVG} alt="sort" />
+      </div>
     </div>
   )
 }
