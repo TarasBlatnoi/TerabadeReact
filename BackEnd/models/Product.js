25eeba4000cfdfdf2019fba92279424abb5bba17
Taras Yurchenko <turchenko730@gmail.com>
2024-08-12 16:12:23 +0300

    feat: made logic for retrieving images from db

--- BackEnd/models/Product.js
+++ BackEnd/models/Product.js
@@ -1,15 +1,34 @@
 const db = require("../database")
 const { imageToBase64 } = require("../utils/imageToBase64")
 class Product {
-  static sqlQueries = {
+  static sql = {
+    createPlaceholders(quantity) {
+      let placeHoldersString = "?"
+      for (let i = 0; i < quantity; i++) {
+        if (i === quantity - 1) {
+          placeHoldersString + "?"
+          return placeHoldersString
+        }
+        placeHoldersString += ", ?"
+      }
+    },
+    findImagesForProducts: `
+        SELECT ImageURL, ImageOrder, Images.ProductID 
+        FROM 
+        product JOIN Images 
+        ON product.ProductID = Images.ProductID
+        WHERE product.ProductID IN (PLACE_HOLDER)
+        ORDER BY Images.ProductID, ImageOrder;
+    `,
     findAll: `
         SELECT * 
-        FROM product;
+        FROM terabade.product;
         `,
     findMenProducts: `
-        SELECT * 
+        SELECT  *
         FROM terabade.product
-        WHERE sex = "men";
+        WHERE sex = "men"
+        LIMIT 9;
         `,
     findWomenProducts: `
         SELECT * 
@@ -26,6 +45,16 @@ class Product {
         FROM terabade.product 
         WHERE ProductID = ?;
     `,
+  }
+
+  static bindImagesToProducts(products, images) {
+    const ProductsWithImages = products.map((product) => {
+      product.images = images.filter(
+        (image) => product.ProductID === image.ProductID,
+      )
+      return product
+    })
+    return ProductsWithImages
   }
 
   static async commitQuery(sql, data) {
@@ -45,36 +74,47 @@ class Product {
   }
 
   static async findAllProducts() {
-    const products = await Product.commitQuery(Product.sqlQueries.findAll)
+    const products = await Product.commitQuery(Product.sql.findAll)
     return products
   }
   static async findMenProducts() {
-    const menProducts = await Product.commitQuery(
-      Product.sqlQueries.findMenProducts,
+    const menProducts = await Product.commitQuery(Product.sql.findMenProducts)
+    const images = await Product.findImagesForProducts(menProducts)
+    const menProductsWithImages = Product.bindImagesToProducts(
+      menProducts,
+      images,
     )
-    return menProducts
+    return menProductsWithImages
   }
 
   static async findWomenProducts() {
     const womenProducts = await Product.commitQuery(
-      Product.sqlQueries.findWomenProducts,
+      Product.sql.findWomenProducts,
     )
     return womenProducts
   }
   static async findChildrenPoducts() {
     const childrenProducts = await Product.commitQuery(
-      Product.sqlQueries.findChildrenPoducts,
+      Product.sql.findChildrenPoducts,
     )
     return childrenProducts
   }
   static async findById(id) {
     const dataForDB = []
     dataForDB.push(id)
-    const product = await Product.commitQuery(
-      Product.sqlQueries.findById,
-      dataForDB,
-    )
+    const product = await Product.commitQuery(Product.sql.findById, dataForDB)
     return product
+  }
+
+  static async findImagesForProducts(products) {
+    const productIds = products.map((product) => product.ProductID)
+    const placeHolders = Product.sql.createPlaceholders(products.length)
+    const queryWithPlaceholders = Product.sql.findImagesForProducts.replace(
+      "PLACE_HOLDER",
+      placeHolders,
+    )
+    const images = await Product.commitQuery(queryWithPlaceholders, productIds)
+    return images
   }
 }
 
