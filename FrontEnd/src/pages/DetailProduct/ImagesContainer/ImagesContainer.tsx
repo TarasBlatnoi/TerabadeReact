0549dd493055a00d71a7b563347fab7dcffa20fa
Taras Yurchenko <turchenko730@gmail.com>
2024-08-14 17:05:02 +0300

    feat: made changing of main image on hover

--- FrontEnd/src/pages/DetailProduct/ImagesContainer/ImagesContainer.tsx
+++ FrontEnd/src/pages/DetailProduct/ImagesContainer/ImagesContainer.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { ImageObject, DetailProductType } from "../../../types"
 import styles from "./ImagesContainer.module.css"
 
@@ -6,25 +7,38 @@ interface ImagesContainerProps {
 }
 
 const ImagesContainer = ({ detailProduct }: ImagesContainerProps) => {
+  const [activeImage, setActiveImage] = useState<string>(
+    detailProduct.images[0].ImageURL
+  )
+
   return (
     <div className={styles.productImagesContainer}>
       <div className={styles.smallImages}>
-        {detailProduct.images.map((image: ImageObject) => {
-          return (
-            <img
-              key={image.ImageOrder}
-              src={`${image.ImageURL}`}
-              alt={detailProduct.name}
-              className={styles.smallImage}
-            />
-          )
-        })}
+        {detailProduct.images.map((image: ImageObject) => (
+          <img
+            key={image.ImageOrder}
+            src={image.ImageURL}
+            alt={detailProduct.name}
+            onMouseEnter={() => {
+              setActiveImage(image.ImageURL)
+            }}
+            className={styles.smallImage}
+          />
+        ))}
       </div>
-      <img
-        src={`${detailProduct.images[0].ImageURL}`}
-        alt={detailProduct.name}
-        className={styles.mainImage}
-      />
+      {detailProduct.images.map((image: ImageObject) => (
+        <img
+          key={image.ImageOrder}
+          src={image.ImageURL}
+          alt={detailProduct.name}
+          onMouseEnter={() => {
+            setActiveImage(image.ImageURL)
+          }}
+          className={`${
+            activeImage === image.ImageURL ? styles.mainImage : styles.hidden
+          }`}
+        />
+      ))}
     </div>
   )
 }
