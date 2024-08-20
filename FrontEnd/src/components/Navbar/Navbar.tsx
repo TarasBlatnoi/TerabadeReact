cee681d765c866b1e0d7dfc1dfe7700428102002
Taras Yurchenko <turchenko730@gmail.com>
2024-08-20 16:30:33 +0300

    feat: made menu for small screens

--- FrontEnd/src/components/Navbar/Navbar.tsx
+++ FrontEnd/src/components/Navbar/Navbar.tsx
@@ -3,15 +3,27 @@ import NavbarSearch from "./NavbarSearch/NavbarSearch"
 import NavbarItemList from "./NavbarItemList/NavbarItemList"
 import NavbarIcons from "./NavbarIcons/NavbarIcons"
 import styles from "./Navbar.module.css"
+import useWindowSize from "../../hooks/useWindowSize"
 
-const Navbar = () => {
+import { SetStateAction, Dispatch } from "react"
+
+interface NavbarProps {
+  menuIconClicked: boolean
+  setMenuIconClicked: Dispatch<SetStateAction<boolean>>
+}
+
+const Navbar = ({ menuIconClicked, setMenuIconClicked }: NavbarProps) => {
+  const windowSize = useWindowSize()
   return (
     <nav className={styles.navbarSmall}>
       <NavbarLogo />
-      <NavbarSearch />
-      <NavbarItemList />
+      {windowSize > 1200 ? <NavbarSearch /> : null}
+      <NavbarItemList
+        menuIconClicked={menuIconClicked}
+        setMenuIconClicked={setMenuIconClicked}
+      />
       <NavbarIcons />
-      <p className={styles.language}>UA</p>
+      {windowSize > 1200 ? <p className={styles.language}>UA</p> : null}
     </nav>
   )
 }
