d7c194b7c3359bc20c0925cb720d156cdece5670
Strelia Illia <seylvin2005@gmail.com>
2024-08-26 17:53:38 +0300

    Fix links in home page

--- FrontEnd/src/components/Offer/Offer.tsx
+++ FrontEnd/src/components/Offer/Offer.tsx
@@ -2,7 +2,9 @@ import styles from "./Offer.module.css"
 import Button from "../../components/UI/Button/Button"
 import skaterPhoto from "../../assets/images/Boy1.svg"
 import ImageStateful from "../UI/ImageStateful/ImageStateful"
+import { useNavigate } from "react-router-dom"
 function Offer() {
+  const navigate = useNavigate()
   return (
     <div className={styles.offer}>
       <h1 className={styles.offerText}>
@@ -11,7 +13,11 @@ function Offer() {
         ПРОДАЖУ
       </h1>
       <div className={styles.orderButtonWrapper}>
-        <Button variant="primary" className={styles.orderButton}>
+        <Button
+          variant="primary"
+          className={styles.orderButton}
+          onClick={() => navigate("/products")}
+        >
           Замовити
         </Button>
         <ImageStateful
